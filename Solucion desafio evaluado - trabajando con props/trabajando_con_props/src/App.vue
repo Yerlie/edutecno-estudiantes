2e3a626a4248e266e1d8343426e022cd28933c43
Yerlie Soussa <75313991+Yerlie@users.noreply.github.com>
2022-06-24 13:16:42 -0400

    Desafío evaluado - Trabajando con Props

---
+++ Solucion desafio evaluado - trabajando con props/trabajando_con_props/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h1>Tarea</h1>
+    <input type="text" v-model="tarea" placeholder="Ingresa nueva tarea" >
+    <button @click="addItem">Añade</button>
+    <ul>
+      <li v-for="(item,index) in lista" :key="index">
+      <PropsVue :prop1="item" @clickElimina="clickHijo"/>
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+import PropsVue from './components/PropsVue.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PropsVue
+  },
+  data(){
+    return {
+      tarea: '',
+      lista: []
+    }
+  },
+  methods: {
+    addItem(){
+      this.lista.push(this.tarea)
+      this.tarea = ''
+    },
+    clickHijo(item){
+      this.lista.splice(this.lista.indexOf(item),1)
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Solucion desafio evaluado - trabajando con props/trabajando_con_props/src/components/PropsVue.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+{{prop1}}
+<button @click="clickElimina">Eliminar</button>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        prop1: {
+            type: String,
+            required: true
+        }
+        },
+    methods: {
+        clickElimina(){
+            this.$emit('clickElimina', this.prop1)
+        }
+}
+}
+</script>
+<style scoped>
+
+</style>>
+
